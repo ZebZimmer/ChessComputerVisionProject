ab9869c4f315275f6ab9902fa0b0f7ddd0e41f37
Zeb <38386036+ZebZimmer@users.noreply.github.com>
2023-10-17 18:38:30 -0500

    'working' CNN that poorly classifies pieces

---
+++ CNN.py
@@ -0,0 +1,28 @@
+import tensorflow
+from tensorflow.keras import layers, models
+
+
+class ChessCNN:
+    def __init__(self, input_dim_x, input_dim_y):
+        self.model = models.Sequential()
+        self.model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(input_dim_x, input_dim_y, 3)))
+        self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D((2, 2)))
+        self.model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(64, activation='relu'))
+
+        # Output layer
+        self.model.add(layers.Dense(12, activation='softmax'))
+
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def train(self, train_images, train_labels, validation_images, validation_labels, epochs=31):
+        self.model.fit(train_images, train_labels, epochs=epochs, validation_data=(validation_images, validation_labels))
+
+    def test(self, test_images, test_labels):
+        test_loss, test_acc = self.model.evaluate(test_images, test_labels)
+        print(f"Test accuracy: {test_acc}")
+        print(f"Test loss: {test_loss}")
---
+++ main.py
@@ -0,0 +1,60 @@
+from PIL import Image
+import numpy as np
+import csv
+from CNN import ChessCNN
+from tqdm import tqdm
+import tensorflow as tf
+
+datasets_filepath = r'C:\Users\zebzi\Documents\School\Master_Year\CSCI 5525\Project\CSV_Multiclass'
+
+def get_data_return_numpy_array(type: str) -> (np.array, np.array):
+    """
+    Get the type (train, test, valid) of the data and return it as two arrays
+    The first is an array of arrays where each index holds data for an image
+    The second array holds the data labels
+    """
+    # labels_dict = {}
+    photos_array = []
+    labels_array = []
+
+    with open(datasets_filepath + f"\{type}\_classes.csv") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in tqdm(csvreader):
+            if(row[0] != "filename"):
+                # labels_dict[row[0]] = np.array(row[2:], int)
+                image = Image.open(datasets_filepath + f"\{type}\\" + row[0])
+                photos_array.append(np.array(image.resize((100, 180))))
+                labels_array.append(np.array(row[2:], int))
+
+
+    # print(labels_dict["c4943d83c06a12ad5e0399d19514a4ca_jpg.rf.8b0040b3b68009f6f700ea28fb1aa491.jpg"])
+    return (np.array(photos_array), np.array(labels_array))
+        
+
+
+def main():
+    # Get the data
+    x_train, y_train = get_data_return_numpy_array("train")
+    x_valid, y_valid = get_data_return_numpy_array("valid")
+    x_test, y_test   = get_data_return_numpy_array("test")
+
+    print(x_train[1].shape)
+    print(x_valid[2].shape)
+
+    # Create the model using the dimensions of the first image (hopefully they're all the same?)
+    model = ChessCNN(x_train[0].shape[0], x_train[0].shape[1])
+
+    # Train the model
+    model.train(x_train, y_train, x_valid, y_valid, 30)
+    
+    # Test the model
+    model.test(x_test, y_test)
+    
+if __name__ == "__main__":
+    print(tf.config.list_physical_devices('GPU'))
+    # Check if TensorFlow is using the GPU
+    if tf.test.is_gpu_available():
+        print("GPU is available and TensorFlow is using it.")
+    else:
+        print("No GPU is available, TensorFlow is using the CPU.")
+    main()
